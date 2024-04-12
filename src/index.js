75f995f8fbcb5ab8d58d4f3d338e4c041adee64b
Phong Nguyen <nnphong1904@gmail.com>
2024-04-12 22:07:29 +1000

    Fixed

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+// import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
+import { createRoot } from "react-dom/client";
 
-ReactDOM.render(
+const rootElement = document.getElementById('root');
+const root = createRoot(rootElement);
+root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-  document.getElementById("root")
+  // document.getElementById("root")
 );
+
 function print() {
   return <p> hello world</p>;
 }
